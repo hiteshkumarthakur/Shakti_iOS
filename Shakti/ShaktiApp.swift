c541988c4614bdbe268836f86e7c6c52e643f216
Hitesh Kumar <hitesh.kumar@phonepe.com>
2026-02-19 21:28:12 +0530

    Initial Commit

---
+++ Shakti/ShaktiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShaktiApp.swift
+//  Shakti
+//
+//  Created by Hitesh Kumar on 19/02/26.
+//
+
+import SwiftUI
+
+@main
+struct ShaktiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
